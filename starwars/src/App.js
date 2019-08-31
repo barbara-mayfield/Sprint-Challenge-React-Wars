68a294586b1f17e83ed9697cb88fe9ce7c372c96
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-30 20:25:30 -0500

    added a styled container to the app

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import Card from './components/CardComponents/Card'
 import './App.css';
+
+const StyledContainer = styled.div`
+  width: 8rem;
+  height: 10rem;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,10 +19,9 @@ const App = () => {
     axios.get('https://swapi.co/api/people/')
           .then(response => {
             console.log(response.data);
-            const person = response.data.results;
-            setPerson(person);
+            setPerson(response.data.results);
       });
-  }, [person]);
+  }, []);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -26,16 +30,17 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <StyledContainer>
       { person.map((people, index) => {
         return (
         <Card
               key = {index}
               name = {people.name}
               birthYear = {people.birth_year}
-              gender = {people.gender}
-              people = {people}/>
+              gender = {people.gender} />
                 )
       })}
+      </StyledContainer>
     </div>
   );
 }
